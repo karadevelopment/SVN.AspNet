9db5e752040fdd6db4c1c286f147884b8a715e7a
svndevelopment <47905966+svndevelopment@users.noreply.github.com>
2019-04-07 04:11:54 +0200

    2.5.6.0

--- Assets/Scripts/SVN/dataTables.js
+++ Assets/Scripts/SVN/dataTables.js
@@ -3,6 +3,7 @@
 
     // ------------------------------------------
 
+    vm.dataTable = null;
     vm.columnsVM = ko.observableArray();
 
     // ------------------------------------------
@@ -11,11 +12,14 @@
         settings.binding = settings.binding || function (x) { return x; };
         settings.ajaxBefore = settings.ajaxBefore || function (x) { };
         settings.ajaxAfter = settings.ajaxAfter || function (x) { };
+        settings.bLengthChange = settings.bLengthChange || false;
+        settings.bAutoWidth = settings.bAutoWidth || false;
 
         for (let i = 1; i <= settings.columns.length; i++) {
             let column = settings.columns[i - 1];
             column.data = column.data || "";
             column.text = column.text || "";
+            column.width = column.width || "auto";
             column.orderable = column.orderable || false;
         }
 
@@ -42,12 +46,13 @@
     };
 
     vm.initDataTable = function () {
-        $("#" + selector).DataTable({
+        vm.dataTable = $("#" + selector).DataTable({
             columns: settings.columns.select(x => {
                 return {
                     data: x.data,
                     defaultContent: "",
                     text: x.text,
+                    width: x.width,
                     orderable: x.orderable,
                 };
             }),
@@ -65,6 +70,7 @@
                 complete: (jqXHR, textStatus) => {
                     let data = jqXHR.responseJSON;
                     settings.ajaxAfter(data);
+                    vm.dataTable.columns.adjust();
                 },
             },
             fnRowCallback: (row, data, colIndex) => {
@@ -89,9 +95,9 @@
             processing: true,
             serverSide: true,
             searching: false,
-            bLengthChange: false,
+            bLengthChange: settings.bLengthChange,
             bInfo: false,
-            bAutoWidth: false,
+            bAutoWidth: settings.bAutoWidth,
         });
     };
 
